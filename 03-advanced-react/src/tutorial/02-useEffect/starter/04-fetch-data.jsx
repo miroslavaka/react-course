a9052dbaee03a46bbecabaec95c44395c79a9434
miroslavaka <kacmarova.miroslava@gmail.com>
2023-02-25 21:20:56 +0100

    fetch data completed

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,37 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('data:', data);
+        setUsers(data);
+        console.log('users:', users);
+      });
+  }, []);
+  return (
+    <>
+      <h2>fetch data example</h2>
+      <h3>Github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { avatar_url, login, url } = user;
+
+          return (
+            <li>
+              <img className="img" src={avatar_url} />
+              <h5>{login}</h5>
+              <a href={url}>Profile</a>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 export default FetchData;
