250ad5ab982fcbac0e74626b37ba540d6be8562a
miroslavaka <kacmarova.miroslava@gmail.com>
2023-02-27 19:30:40 +0100

    short circuit react

--- 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ 03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,7 +8,44 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <div>
+      <h4>{text || 'default value'}</h4>
+      {text && (
+        <div>
+          <h2>whatever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      {/* {!text && (
+        <div>
+          <h2>whatever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )} */}
+      {user && <SomeComponent name={user.name} />}
+
+      {/* TERNARY OPERATOR: */}
+      {user ? (
+        <div>
+          <h4>hello there user {user.name}</h4>
+        </div>
+      ) : (
+        <div>
+          <h2>please login</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const SomeComponent = ({ name }) => {
+  return (
+    <div>
+      <h2>whatever return</h2>
+      <h2>{name}</h2>
+    </div>
+  );
 };
 
 export default ShortCircuitExamples;
